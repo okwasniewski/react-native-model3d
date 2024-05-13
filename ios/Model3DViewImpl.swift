411d6a93347e620897951ea9917435fb0bbede85
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-05-13 09:31:00 +0200

    feat: allow to compile on iOS

--- ios/Model3DViewImpl.swift
+++ ios/Model3DViewImpl.swift
@@ -2,17 +2,14 @@ import SwiftUI
 import RealityKit
 import Observation
 
-#if os(visionOS)
+class Model3DViewProps: ObservableObject {
+  @Published var url: String?
+  @Published var aspectRatio: NSString?
 
-@Observable
-class Model3DViewProps {
-  var url: String?
-  var aspectRatio: NSString?
-  
   init(url: String?) {
     self.url = url
   }
-  
+
   func convertAspectRatio(aspectRatio: NSString?) -> ContentMode {
     if aspectRatio == "fill" {
       return .fill
@@ -23,7 +20,8 @@ class Model3DViewProps {
 }
 
 struct ModelView: View {
-  @Environment(Model3DViewProps.self) private var props
+  @EnvironmentObject private var props: Model3DViewProps
+
   
   var body: some View {
     if let url = props.url {
@@ -38,6 +36,7 @@ struct ModelView: View {
   }
 }
 
+#if os(visionOS)
 extension Model3D {
   public init<Model, Placeholder>(maybeURL: String, @ViewBuilder content: @escaping (ResolvedModel3D) -> Model, @ViewBuilder placeholder: @escaping () -> Placeholder) where Content == Model3DPlaceholderContent<Model, Placeholder>, Model : View, Placeholder : View {
     if let url = URL(string: maybeURL), maybeURL.contains("http") {
@@ -47,5 +46,6 @@ extension Model3D {
     }
   }
 }
-
 #endif
+
+
